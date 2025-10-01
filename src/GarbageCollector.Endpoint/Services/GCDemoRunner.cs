e384d4f42323fcc662e996d696746874451da623
Alireza <a.z.nikravesh@gmail.comm>
2025-10-01 17:08:27 +0330

    added GC demo runner

---
+++ src/GarbageCollector.Endpoint/Services/GCDemoRunner.cs
@@ -0,0 +1,15 @@
+﻿namespace GarbageCollector.Endpoint.Services;
+public class GCDemoRunner
+{
+    public void RunAll()
+    {
+        Console.WriteLine("=== GC Generations Demo ===");
+        Console.WriteLine($".NET Version: {Environment.Version}\n");
+
+        new Gen0Demo().Run();
+        new Gen1Demo().Run();
+        new Gen2Demo().Run();
+
+        Console.WriteLine("\n=== Demo Finished ===");
+    }
+}
