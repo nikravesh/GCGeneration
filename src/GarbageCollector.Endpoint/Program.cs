a29092e47ca48503d051001291f262039d703c12
Alireza <a.z.nikravesh@gmail.comm>
2025-10-01 17:08:57 +0330

    using demo runner

--- src/GarbageCollector.Endpoint/Program.cs
+++ src/GarbageCollector.Endpoint/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GarbageCollector.Endpoint.Services;
+
+var runner = new GCDemoRunner();
+runner.RunAll();
