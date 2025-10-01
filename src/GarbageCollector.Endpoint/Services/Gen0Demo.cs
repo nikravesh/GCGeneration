dffacfa8933d999ff2df29ce01cc9101ed8bf760
Alireza <a.z.nikravesh@gmail.comm>
2025-10-01 17:05:04 +0330

    Add project files.

---
+++ src/GarbageCollector.Endpoint/Services/Gen0Demo.cs
@@ -0,0 +1,22 @@
+﻿using GarbageCollector.Endpoint.Utils;
+
+namespace GarbageCollector.Endpoint.Services;
+public class Gen0Demo
+{
+    public void Run()
+    {
+        Console.WriteLine("-- Generation 0 Demo --");
+
+        var obj1 = new object();
+        var obj2 = new object();
+
+        MemoryPrinter.PrintGenerations(obj1, obj2);
+
+        GC.Collect(0, GCCollectionMode.Forced);
+        GC.WaitForPendingFinalizers();
+
+        MemoryPrinter.PrintGenerations(obj1, obj2);
+        MemoryPrinter.PrintCollectionCounts();
+        Console.WriteLine();
+    }
+}
---
+++ src/GarbageCollector.Endpoint/Utils/MemoryPrinter.cs
@@ -0,0 +1,20 @@
+﻿namespace GarbageCollector.Endpoint.Utils;
+public class MemoryPrinter
+{
+    public static void PrintGenerations(params object[] objects)
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            Console.WriteLine($"Object {i + 1} is in generation: {GC.GetGeneration(objects[i])}");
+        }
+    }
+
+    public static void PrintCollectionCounts()
+    {
+        Console.WriteLine("GC Collection Counts:");
+        for (int i = 0; i <= GC.MaxGeneration; i++)
+        {
+            Console.WriteLine($"Generation {i}: {GC.CollectionCount(i)} collections");
+        }
+    }
+}
