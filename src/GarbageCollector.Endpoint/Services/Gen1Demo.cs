c838fb1739b39db9f57161c697e99acf908a3f2a
Alireza <a.z.nikravesh@gmail.comm>
2025-10-01 17:06:30 +0330

    added generation 1

---
+++ src/GarbageCollector.Endpoint/Services/Gen1Demo.cs
@@ -0,0 +1,20 @@
+﻿using GarbageCollector.Endpoint.Utils;
+
+namespace GarbageCollector.Endpoint.Services;
+public class Gen1Demo
+{
+    public void Run()
+    {
+        Console.WriteLine("-- Generation 1 Demo --");
+
+        var arr = new byte[10000];
+        MemoryPrinter.PrintGenerations(arr);
+
+        GC.Collect(1, GCCollectionMode.Forced);
+        GC.WaitForPendingFinalizers();
+
+        MemoryPrinter.PrintGenerations(arr);
+        MemoryPrinter.PrintCollectionCounts();
+        Console.WriteLine();
+    }
+}
